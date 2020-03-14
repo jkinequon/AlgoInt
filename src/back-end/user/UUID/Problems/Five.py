2aca422e5609908e94fc8d5f88f5b1d18fbc6906
Justice Sparvier-Kinequon <justicekinequon@gmail.com>
2020-03-14 17:30:50 -0600

    Added updated Dockerfile and a DockerTest.py that is empty, as well as the user folder

---
+++ src/back-end/user/UUID/Problems/Five.py
@@ -0,0 +1,67 @@
+from AlgoInt.Problems import FiveS
+import multiprocessing
+import time
+
+class Testing:
+    def __init__(self):
+        self.total = 0
+
+    def test_case_1(self):
+        nums1 = [1,3]
+        nums2 = [2]
+        if FiveS.median(nums1, nums2) != 2.0:
+            return 1
+        else:
+            return 0
+
+    def test_case_2(self):
+        nums1 = [1, 2]
+        nums2 = [3, 4, 5]
+        if FiveS.median(nums1, nums2) != 3:
+            return 1
+        else:
+            return 0
+
+    def test_case_3(self):
+        nums1 = [1, 2, 3, 4]
+        nums2 = [3, 4, 5, 6]
+        if FiveS.median(nums1, nums2) != 7:
+            return 1
+        else:
+            return 0
+
+    def test_case_4(self):
+        nums1 = [1, 2]
+        nums2 = [3, 4]
+        if FiveS.median(nums1, nums2) != 2.5:
+            return 1
+        else:
+            return 0
+
+    def test_case_5(self):
+        nums1 = [10, 11, 12]
+        nums2 = [9, 10, 11]
+        if FiveS.median(nums1, nums2) != 10.5:
+            return 1
+        else:
+            return 0
+
+    def runAllTests(self, proc):
+        tests = [self.test_case_1, self.test_case_2, self.test_case_3, self.test_case_4, self.test_case_5]
+        for test in tests:
+            self.total = test()
+        proc.put(self.total)
+        return self.total
+
+def runTests():
+    testing = Testing()
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(target=testing.runAllTests, args=(q,))
+    p.start()
+    time.sleep(10)
+    p.join()
+    p.terminate()
+    return q.get()
+
+
+print(runTests())
---
+++ src/back-end/user/UUID/Problems/Two.py
@@ -0,0 +1,51 @@
+from AlgoInt.Problems import TwoS
+import multiprocessing
+import time
+
+class Testing:
+    def __init__(self):
+        self.total = 0
+
+    def test_case_1(self):
+        if TwoS.mostCommon([1]) != 1:
+            return 1
+        else:
+            return 0
+
+    def test_case_2(self):
+        if TwoS.mostCommon([2, 1, 2]) != 2:
+            return 1
+        else:
+            return 0
+
+    def test_case_3(self):
+        if TwoS.mostCommon([0, 0, 1, 1, 1]) != 1:
+            return 1
+        else:
+            return 0
+
+    def test_case_4(self):
+        if TwoS.mostCommon([3, 3, 3, 3, 3, 3]) != 3:
+            return 1
+        else:
+            return 0
+
+    def runAllTests(self, proc):
+        tests = [self.test_case_1, self.test_case_2, self.test_case_3, self.test_case_4]
+        for test in tests:
+            self.total = test()
+        proc.put(self.total)
+        return self.total
+
+def runTests():
+    testing = Testing()
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(target=testing.runAllTests, args=(q,))
+    p.start()
+    time.sleep(10)
+    p.join()
+    p.terminate()
+    return q.get()
+
+
+print(runTests())
