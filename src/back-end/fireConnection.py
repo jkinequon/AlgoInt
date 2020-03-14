9e194dd6c1ccecf150c3d1953ab5d8aa6ff97c25
Jeremy <JeremyStorring@gmail.com>
2020-03-14 15:54:23 -0600

    Created the firebase connection file as a database class

---
+++ src/back-end/fireConnection.py
@@ -0,0 +1,22 @@
+import requests
+from firebase import firebase
+
+
+class Database:
+    def __init__(self):
+        self.firebase = firebase.FirebaseApplication("https://onlineide-89276.firebaseio.com/", None)
+
+    def getRankings(self):
+        return
+
+    def updateRankings(self):
+        return
+
+    def getChallenge(self):
+        return
+
+    def createRankingObject(self):
+        return
+
+    def createChallengeObject(self):
+        return
