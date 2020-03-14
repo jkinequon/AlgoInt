f4a4173804ccdad24ac674e48b7857ff538524d6
Jeremy <JeremyStorring@gmail.com>
2020-03-14 15:35:13 -0600

    Added functions for handling docker

---
+++ src/back-end/DockerHandler.py
@@ -0,0 +1,22 @@
+def DockerRun():
+    return
+
+
+def getErrorData():
+    return
+
+
+def getTestsDone():
+    return
+
+
+def getOutputData():
+    return
+
+
+def handleInformation():
+    return
+
+
+def DockerClean():
+    return
