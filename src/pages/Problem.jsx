b9ffdbd1fd5fe6037e9eb12efcd452dcff0a017a
Ricardo Rheeder <ricardorheeder@gmail.com>
2020-03-17 01:35:56 -0600

    Added hint functionality and modals

--- src/pages/Problem.jsx
+++ src/pages/Problem.jsx
@@ -1,32 +1,101 @@
 import React, { Component } from 'react'
 
 import { CodeEditor } from '../components'
+import ReactModal from 'react-modal'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { setCurrentQuestion, setQuestionQueue } from '../redux/actions/actions'
 
-class Problem extends Component {
-    render() {
-        const { setCurrentQuestion } = this.props;
 
+
+class Problem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          questionTitle: '999',
+          questionDescription: 'A description of the title',
+          questionHints: '',
+          showHint1Modal: false,
+          showHint2Modal: false
+        };
+      }
+
+    handleOpenHint1Modal = () => {
+    this.setState({ showHint1Modal: true });
+    }
+    
+    handleOpenHint2Modal = () => {
+    this.setState({ showHint2Modal: true });
+    }
+    
+    handleCloseHint1Modal = () => {
+    this.setState({ showHint1Modal: false });
+    }
+    
+    handleCloseHint2Modal = () => {
+    this.setState({ showHint2Modal: false });
+    }
+
+    componentDidMount(){
+        const { setCurrentQuestion, questionsObject, currentQuestion } = this.props;
+        var q_object = questionsObject[currentQuestion]
+        this.setState({
+            questionTitle: q_object['Question Name'],
+            questionDescription: q_object['Question Description'],
+            questionHints: q_object['Question Hints']
+          });
+        }
+        
+    render() {
+        const { setCurrentQuestion, questionsObject } = this.props;
+        console.log(this.state.questionHints)
         return (
             <div className="parent-container">
+               {/* HINT MODALS */}
+               <ReactModal 
+                    isOpen={this.state.showHint1Modal}
+                    contentLabel="Modal #1 Global Style Override Example"
+                    onRequestClose={this.handleCloseHint1Modal}
+                    className="Modal"
+                    overlayClassName="Overlay"
+                >
+                    <div className='modal-div'>
+                        <h1>{this.state.questionHints[0]}</h1>
+                        <button className='problem-button modal-close' onClick={this.handleCloseHint1Modal}><span>Close Modal</span></button>
+                    </div>
+                </ReactModal>
+                <ReactModal 
+                    isOpen={this.state.showHint2Modal}
+                    contentLabel="Modal #2 Global Style Override Example"
+                    onRequestClose={this.handleCloseHint2Modal}
+                    className="Modal"
+                    overlayClassName="Overlay"
+                >
+                    <div className='modal-div'>
+                        <h1>{this.state.questionHints[1]}</h1>
+                        <button className='problem-button modal-close' onClick={this.handleCloseHint2Modal}><span>Close Modal</span></button>
+                    </div>
+                </ReactModal>
+                {/* HINT MODALS */}
                 <div className="left-container">
                     <div className="question-div">
                         <h1 className="question-title">
-                            Question 233
+                            Question {this.state.questionTitle}
                         </h1>
                         <h2 className="question-text">
-                            A description of the title
+                            {this.state.questionDescription}
                         </h2>
                     </div>
-
+                    <div className="hint-div">
+                        <button className='problem-button' onClick={() => this.handleOpenHint1Modal()}><span>HINT 1</span></button>
+                        <button className='problem-button' onClick={() => this.handleOpenHint2Modal()}><span>HINT 2</span></button>
+                    </div>
                     <div className="console-div">
                         Console Output
                     </div>
                 </div>
-                <div className="right-container">
+                <div className="right-container" >
                     <CodeEditor/>
                     <div className="bottom-right-bar">
                         <button className='problem-button'><span>RUN</span></button>
@@ -42,6 +111,8 @@ const mapStateToProps = (state) => {
     return {
         currentQuestion: state.delta.currentQuestion,
         questionQueue: state.delta.questionQueue,
+        questionsObject: state.delta.questionsObject,
+
 
     };
 };
