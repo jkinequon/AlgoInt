dfd0f40dffe446d3610ddae7eafd07c4f92db4a7
Justice Sparvier-Kinequon <justicekinequon@gmail.com>
2020-03-19 13:48:06 -0600

    Added reading of testtime in DockerTest

--- src/back-end/DockerTest.py
+++ src/back-end/DockerTest.py
@@ -25,6 +25,7 @@ class DockerTest:
               "/tmp:/tmp --rm -e user="+self.UUID+" -e path_file="+self.QuestionID+".py python-test"
         os.system(cmd)
 
+  command 'nmon' from deb nmon
     def DockerClean(self):
         cmd = "rm -r ./user/" + self.UUID
         os.system(cmd)
@@ -42,10 +43,12 @@ class DockerTest:
         data = []
         for line in file:
             data.append(line.rstrip())
-        self.OutputData = data[:-2]
-        self.TestsFailed = data[-2]
-        self.TestsTotal = data[-1]
-        return
+        self.OutputData = data[:-3]
+        self.TestsFailed = data[-3]
+        self.TestsTotal = data[-2]
+        self.TestTime = data[-1]
+        return  command 'nmon' from deb nmon
+
 
     def getTestsFailed(self):
         return self.TestsFailed
