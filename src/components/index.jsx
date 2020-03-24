a1e9a7ab111ce509e078f9203a2b8fdfde2f9529
Ricardo Rheeder <ricardorheeder@gmail.com>
2020-03-24 14:24:22 -0600

    Applied more styling to components I missed

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,4 @@
 export { default as Navbar } from "./Navbar";
-export { default as Sidebar } from "./Sidebar/";
 export { default as Challenge } from "./Challenge";
 export { default as HomeOptions } from "./HomeOptions";
 export { default as SelectionOption } from "./SelectionOption";
