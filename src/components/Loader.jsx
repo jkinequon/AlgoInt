5075b4253f7cb5edf4655e7bd932ffb8e730482d
Ricardo Rheeder <ricardorheeder@gmail.com>
2020-03-26 01:01:39 -0600

    Finished loading screen - it was the fucking Zoom:300% that did the trick, GG

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -9,6 +9,22 @@ export default class Loader extends Component {
         className="loader-modal"
         overlayClassName="loader-modal-overlay"
       >
+        <section className="loading-container">
+          <div class="loading loading06">
+            <span data-text="C">C</span>
+            <span data-text="O">O</span>
+            <span data-text="M">M</span>
+            <span data-text="P">P</span>
+            <span data-text="I">I</span>
+            <span data-text="L">L</span>
+            <span data-text="I">I</span>
+            <span data-text="N">N</span>
+            <span data-text="G">G</span>
+            <span data-text=".">.</span>
+            <span data-text=".">.</span>
+            <span data-text=".">.</span>
+          </div>
+        </section>
         <div class="loader triangle">
           <svg viewBox="0 0 86 80">
             <polygon points="43 8 79 72 7 72"> </polygon>
