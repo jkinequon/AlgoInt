19d05c9ede5920de5c667a0f7fcbf58d1c3e6539
Ricardo Rheeder <ricardorheeder@gmail.com>
2020-03-25 20:06:10 -0600

    Removed HelloWorld from problem list

--- src/pages/Selection.jsx
+++ src/pages/Selection.jsx
@@ -1,51 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { SelectionOption } from '../components';
-import { connect } from 'react-redux'
-
+import { SelectionOption } from "../components";
+import { connect } from "react-redux";
 
 class Selection extends Component {
-
-    returnSelections = (questionsObject) => 
+  returnSelections = questionsObject =>
     questionsObject.map((val, i) => {
-        console.log(val)
-
+      // console.log(val)
+      if (i != 0)
         return (
-            <SelectionOption 
-                difficulty={val['Question Difficulty']} 
-                type={val['Question Type']} 
-                language={val['Language']} 
-                description={val['Question Name']}
-                number={val['QuestionID']}
-            />
-        )
-    })
+          <SelectionOption
+            difficulty={val["Question Difficulty"]}
+            type={val["Question Type"]}
+            language={val["Language"]}
+            description={val["Question Name"]}
+            number={val["QuestionID"]}
+          />
+        );
+    });
 
-    render() {
-        const {questionsObject} = this.props
+  render() {
+    const { questionsObject } = this.props;
 
-        return (
-            <div className="container">
-                <div className="react-table-root">
-                    <div className="table-header">
-                        {
-                            ["Difficulty", "Type", "Language", "Description", "Rankings"].map((e, i) => {
-                                return(<div className="header-item" key={i}>{e}</div>)   
-                            })
-                        }
-                    </div>
-                {this.returnSelections(questionsObject)}    
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="container">
+        <div className="react-table-root">
+          <div className="table-header">
+            {["Difficulty", "Type", "Language", "Description", "Rankings"].map(
+              (e, i) => {
+                return (
+                  <div className="header-item" key={i}>
+                    {e}
+                  </div>
+                );
+              }
+            )}
+          </div>
+          {this.returnSelections(questionsObject)}
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        questionsObject: state.delta.questionsObject,
-
-    };
+const mapStateToProps = state => {
+  return {
+    questionsObject: state.delta.questionsObject
+  };
 };
 
 export default connect(mapStateToProps, null)(Selection);
