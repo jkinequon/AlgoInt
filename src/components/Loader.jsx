f93857264bd074226e7d312105850dd4c241e3b0
Ricardo Rheeder <ricardorheeder@gmail.com>
2020-03-26 00:46:24 -0600

    Loader implemented

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
 
-
 export default class Loader extends Component {
   render() {
     return (
-        <ReactModal
+      <ReactModal
         isOpen={true}
         className="loader-modal"
         overlayClassName="loader-modal-overlay"
       >
         <div class="loader triangle">
-        <svg viewBox="0 0 86 80">
-          <polygon points="43 8 79 72 7 72"> </polygon>
-        </svg>
-      </div>
+          <svg viewBox="0 0 86 80">
+            <polygon points="43 8 79 72 7 72"> </polygon>
+          </svg>
+        </div>
       </ReactModal>
-     
     );
   }
 }
