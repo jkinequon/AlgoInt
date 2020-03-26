8eae921e9fd368f89d4710a2e704354e3de5925c
Ricardo Rheeder <ricardorheeder@gmail.com>
2020-03-25 23:58:56 -0600

    IT DIDNT PUSH!?

---
+++ src/components/Loader.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import ReactModal from "react-modal";
+
+
+export default class Loader extends Component {
+  render() {
+    return (
+        <ReactModal
+        isOpen={true}
+        className="loader-modal"
+        overlayClassName="loader-modal-overlay"
+      >
+        <div class="loader triangle">
+        <svg viewBox="0 0 86 80">
+          <polygon points="43 8 79 72 7 72"> </polygon>
+        </svg>
+      </div>
+      </ReactModal>
+     
+    );
+  }
+}
